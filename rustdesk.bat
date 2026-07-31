0dbc01c85120f078a57e81eb5f6e5127bfcb566a
tpzltn <34416737+tpzltn@users.noreply.github.com>
2026-07-31 09:57:44 +0200

    Update rustdesk.bat

--- rustdesk.bat
+++ rustdesk.bat
@@ -1,26 +1,33 @@
 @echo off
+setlocal
 
+echo.
+set /p rustdesk_cfg=Masold be a RustDesk konfiguracios kodot: 
+echo.
 
-REM Get your config string from your Web portal and Fill Below
-set rustdesk_cfg="0nIw9GduQXZulGcvRnLl12boJiOikXYsVmciwiIiojIpBXYiwiI9E1MNJ2TBdjRwYkaHhjdxUGWMJVWBJmQv50dpVjUyxmVMJGcMZlS2p3dhJnI6ISeltmIsICcvRnL0VmbpB3b05SZt9GaiojI0N3boJye"
+if "%rustdesk_cfg%"=="" (
+    echo Nem adtal meg konfiguracios kodot.
+    pause
+    exit /b 1
+)
 
-REM ############################### Please Do Not Edit Below This Line #########################################
-
-if not exist C:\Temp\ md C:\Temp\
-cd C:\Temp\
+if not exist C:\Temp md C:\Temp
+cd /d C:\Temp
 
 curl -L "https://github.com/rustdesk/rustdesk/releases/download/1.4.9/rustdesk-1.4.9-x86_64.exe" -o rustdesk.exe
 
 rustdesk.exe --silent-install
-timeout /t 20
 
+timeout /t 20 /nobreak
 
 for /f "delims=" %%i in ('rustdesk.exe --get-id ^| more') do set rustdesk_id=%%i
 
-rustdesk.exe --config %rustdesk_cfg%
+rustdesk.exe --config "%rustdesk_cfg%"
 
-echo ...............................................
-REM Show the value of the ID Variable
+echo.
+echo ==========================================
 echo RustDesk ID: %rustdesk_id%
+echo ==========================================
+echo.
 
-echo ...............................................
+pause
